dfa7cf87e0364b3add1db85c275a4e4d9b9de70a
meve_ph <phillippmevenkamp@gmail.com>
2016-04-21 09:01:30 +0200

    add sum functions to vector

--- SimulatedAnnealing/src/de/dhbw/evol/util/Vector6D.java
+++ SimulatedAnnealing/src/de/dhbw/evol/util/Vector6D.java
@@ -5,8 +5,31 @@ import java.util.List;
 
 public class Vector6D {
 	private List<Double> data;
+	private double c = 2 * Math.PI;
 	
-	public Vector6D() {
+	public Vector6D(double c) {
 		data = new ArrayList<>(6);
+		
+		this.c = c;
+	}
+	
+	public double getSquareSum() {
+		double returnValue = 0.0f;
+		
+		for(Double element: data) {
+			returnValue += element * element;
+		}
+		
+		return returnValue;
+	}
+	
+	public double getCosinusSum() {
+		double returnValue = 0.0f;
+		
+		for(Double element : data) {
+			returnValue += Math.cos(c * element);
+		}
+		
+		return returnValue;
 	}
 }
