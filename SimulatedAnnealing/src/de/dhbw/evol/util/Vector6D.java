399b924ff75c8a44e8f5f09e510988e4750d8f61
meve_ph <phillippmevenkamp@gmail.com>
2016-04-21 08:53:16 +0200

    add dependencies

--- SimulatedAnnealing/src/de/dhbw/evol/util/Vector6D.java
+++ SimulatedAnnealing/src/de/dhbw/evol/util/Vector6D.java
@@ -1,9 +1,12 @@
 package de.dhbw.evol.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Vector6D {
 	private List<Double> data;
 	
 	public Vector6D() {
-		
+		data = new ArrayList<>(6);
 	}
 }
