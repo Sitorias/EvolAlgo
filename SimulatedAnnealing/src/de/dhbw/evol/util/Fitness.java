36b32aac40351cdc062b735461ac0818715028db
SvenGoesGit <Sven.Durchholz@googlemail.com>
2016-04-21 09:05:41 +0200

    Added new fitness function deleted metadata

--- SimulatedAnnealing/src/de/dhbw/evol/util/Fitness.java
+++ SimulatedAnnealing/src/de/dhbw/evol/util/Fitness.java
@@ -1,5 +1,25 @@
 package de.dhbw.evol.util;
+import java.math.*;
+import de.dhbw.evol.util.Vector6D;
+
 
 public class Fitness {
-
+	double a,b,c,d;
+	
+	public Fitness(double a, double b, double c, double d){
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.d = d;
+	}
+	
+	public double getFitness(Vector6D parVec){
+		double fitness;
+		
+		fitness = a*Math.pow(Math.E, (-b*parVec.getSquareSum())
+				- Math.pow(Math.E, (1/d)*parVec.getCosSum(c))
+				+a+Math.E);
+		
+		return fitness;
+	}
 }
