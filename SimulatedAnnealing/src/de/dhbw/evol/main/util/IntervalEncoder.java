4ce4944bd417db679e2c6663e903661dcd6cd4e2
meve_ph <phillippmevenkamp@gmail.com>
2016-05-11 14:08:32 +0200

    create IntervalEncoder

---
+++ SimulatedAnnealing/src/de/dhbw/evol/main/util/IntervalEncoder.java
@@ -0,0 +1,11 @@
+package de.dhbw.evol.main.util;
+
+import java.util.Map;
+
+public class IntervalEncoder {
+	Map<Float, Byte[]> containedValues = null;
+	
+	IntervalEncoder(float lowerIntervalBorder, float upperIntervalBorder, float epsilon) {
+		
+	}
+}
