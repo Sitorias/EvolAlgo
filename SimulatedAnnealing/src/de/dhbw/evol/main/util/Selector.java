418bb9cc23d1403a6851b5cf13166addcad26889
meve_ph <phillippmevenkamp@gmail.com>
2016-05-11 16:06:24 +0200

    finish selector

--- SimulatedAnnealing/src/de/dhbw/evol/main/util/Selector.java
+++ SimulatedAnnealing/src/de/dhbw/evol/main/util/Selector.java
@@ -5,13 +5,15 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import javax.xml.crypto.KeySelector.Purpose;
+
 public class Selector {
 	public static Population selectFromPopulation(Population population) {
 		List<Chromosom> survivingIndividuals = new ArrayList<>();
 		
-		float popsize = population.population.size() / 7;
+		int popsize = population.population.size() / 7;
 		
-		Map<Chromosom, Double> probabilityToSurviveForChromosom = new ConcurrentHashMap<>();
+		List<Double> probabilityToSurviveForChromosom = new ArrayList<Double>();
 		
 		double sumFitness = 0;
 		
@@ -22,30 +24,36 @@ public class Selector {
 		for(Chromosom chromosom : population.population) {
 			double probabilityToSurvive = chromosom.getFitness() / sumFitness;
 			
-			probabilityToSurviveForChromosom.put(chromosom, probabilityToSurvive);
+			probabilityToSurviveForChromosom.add(probabilityToSurvive);
 		}		
 		
-		Map<Chromosom, Double> remainingProbability = new ConcurrentHashMap<>();
+		List<Double> remainingProbability = new ArrayList<>();
 		
-		for(Chromosom chromosom : probabilityToSurviveForChromosom.keySet()) {
-			double probability = probabilityToSurviveForChromosom.get(chromosom) * popsize;
+		for(int i = 0; i < probabilityToSurviveForChromosom.size(); i++) {
+			probabilityToSurviveForChromosom.set(i, probabilityToSurviveForChromosom.get(i) * popsize);
 			
-			while(probability > 1.0) {
-				survivingIndividuals.add(chromosom);
-				probability--;
+			while(probabilityToSurviveForChromosom.get(i) > 1.0) {
+				survivingIndividuals.add(population.population.get(i));
+				probabilityToSurviveForChromosom.set(i, probabilityToSurviveForChromosom.get(i) - 1);
 			}
 			
-			remainingProbability.put(chromosom, probability);
+			remainingProbability.set(i, probabilityToSurviveForChromosom.get(i));
 		}
 		
 		double sumProbabilities = 0;
 		
-		for(double probability : remainingProbability.values()) {
+		for(double probability : remainingProbability) {
 			sumProbabilities += probability;
 		}
 		
-		for(double probability : remainingProbability.values()) {
+		for(double probability : remainingProbability) {
 			probability = probability / sumProbabilities;
+		}
+		
+		for(int i = survivingIndividuals.size(); i < popsize; i++) {
+			int indexToAdd = Evolution.getInstance().getRandomIntInInterval(population.population.size());
+			
+			survivingIndividuals.add(population.population.get(indexToAdd));
 		}
 		
 		Population resultingPopulation = new Population();
