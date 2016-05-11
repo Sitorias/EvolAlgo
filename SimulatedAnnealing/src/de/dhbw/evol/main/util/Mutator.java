2ce5c729e3800642c45ab3f778b2b07f7bc8d866
SvenGoesGit <Sven.Durchholz@googlemail.com>
2016-05-11 16:21:06 +0200

    corrected Mutator

--- SimulatedAnnealing/src/de/dhbw/evol/main/util/Mutator.java
+++ SimulatedAnnealing/src/de/dhbw/evol/main/util/Mutator.java
@@ -4,17 +4,18 @@ public final class Mutator {
 	
 	public static Population mutate(Population pop, double chance, int genLen){
 		for (Chromosom chromosom : pop.population){
-			double randomNumber = Evolution.getInstance().getRandomNumberInInterval(0, 1);
-			if (randomNumber <= chance){
-				int mutPos = Evolution.getInstance().getRandomIntInInterval(genLen);
-				if (chromosom.content[mutPos]=='0'){
-					chromosom.content[mutPos]='1';
-				} else {
-					chromosom.content[mutPos]='0';
+			for (int i = 0; i < genLen; i++) { 
+				double randomNumber = Evolution.getInstance().getRandomNumberInInterval(0, 1);
+				if (randomNumber <= chance){
+					if (chromosom.content[i]=='0'){
+						chromosom.content[i]='1';
+					} else {
+						chromosom.content[i]='0';
+					}
 				}
 			}
+			
 		}
-		
 		return pop;
 	}
 }
--- SimulatedAnnealing/src/de/dhbw/evol/main/util/Recombinator.java
+++ SimulatedAnnealing/src/de/dhbw/evol/main/util/Recombinator.java
@@ -1,5 +1,22 @@
 package de.dhbw.evol.main.util;
 
-public class Recombinator {
+public final class Recombinator {
 
+	public static Population recombinate(Population pop, int genLen){
+		for (int i=0; i < pop.population.size(); i++){
+			int point1 = Evolution.getInstance().getRandomIntInInterval(genLen);
+			int point2 = Evolution.getInstance().getRandomIntInInterval(genLen);
+			while (point1 == point2){
+				point2 = Evolution.getInstance().getRandomIntInInterval(genLen);
+			}
+			int start = Math.max(point1, point2);
+			int end = Math.min(point1, point2);
+			
+			for (int j = start; i < end; i++){
+				pop.population.get(i).content[j] = pop.population.get(i+1).content[j];
+				pop.population.get(i+1).content[j] = pop.population.get(i).content[j];
+			}
+		}
+		return pop;
+	}
 }
