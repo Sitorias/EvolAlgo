3898adbeb6898b11c994aa0a4e7288a1c3aeb120
SvenGoesGit <Sven.Durchholz@googlemail.com>
2016-05-11 14:54:11 +0200

    added generateRandomChromosome

--- SimulatedAnnealing/src/de/dhbw/evol/main/GeneticAlgorithm.java
+++ SimulatedAnnealing/src/de/dhbw/evol/main/GeneticAlgorithm.java
@@ -7,11 +7,20 @@ public class GeneticAlgorithm {
 	private static final double b = 0.2;
 	private static final double c = 2 * Math.PI;
 	private static final int d = 6;
-
+	
+	private static final int popSize = 10;
+	
+	
 	public GeneticAlgorithm() {
 	}
 
 	public static void main(String[] args) {
 		Fitness myFitness = new Fitness(a, b, c, d);
+	}
+	
+	private void generateStartPopulation(){
+		for (int i=0; i>popSize; i++){
+			
+		}
 	}
 }
--- SimulatedAnnealing/src/de/dhbw/evol/main/util/Chromosom.java
+++ SimulatedAnnealing/src/de/dhbw/evol/main/util/Chromosom.java
@@ -2,4 +2,8 @@ package de.dhbw.evol.main.util;
 
 public class Chromosom {
 	public char[] content;
+	
+	public Chromosom(char[] content){
+		this.content = content;
+	}
 }
